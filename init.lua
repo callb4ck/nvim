d7d706f5c37ba030c38d6191ba3e4165ae6b9eb8
DumbMahreeo <massi.noviello@gmail.com>
2021-09-02 06:58:44 +0200

    requiring autocmds and removing shell setting

--- init.lua
+++ init.lua
@@ -8,11 +8,11 @@ set("expandtab")
 set("tabstop", 4)
 set("softtabstop", 4)
 set("expandtab")
-set("shell", "/bin/sh")
 set("mouse", "a")
 set("updatetime", 300)
 
 require "plugins"
 require "mappings"
+require "autocmds"
 
 vim.cmd("retab")
