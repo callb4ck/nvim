15eeb0f07fc7433c95f13610670eb4af9f0d3017
DumbMahreeo <massi.noviello@gmail.com>
2021-10-23 02:32:15 +0200

    Added luamap to map directly to lua code

--- lua/vim_interfaces/map.lua
+++ lua/vim_interfaces/map.lua
@@ -4,4 +4,10 @@ function map_function(type)
   end
 end
 
-return {map = map_function}
+function lua_map_function(type)
+  return function(key, cmd)
+    vim.api.nvim_set_keymap(type, key, "<cmd>lua "..cmd.."<CR>", {noremap = true, silent = true})
+  end
+end
+
+return {map = map_function, luamap = lua_map_function}
