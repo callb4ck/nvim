f826cba37f92210f62301bdeb587fa7387497c91
DumbMahreeo <dumbmahreeo@duck.com>
2022-07-03 22:37:09 +0200

    Deprecating CoC

--- lua/language_server_list.lua
+++ lua/language_server_list.lua
@@ -1,15 +1,9 @@
 return {
-  nvimlsp = { -- Names of the language servers:
+  lsp = { -- Names of the language servers:
     "nimls",
     "crystalline",
     "ccls",
     "rust_analyzer",
     "zls",
   },
-  coc = { -- Filetypes
-    --"rust",
-    "go",
-  }
 }
-
--- CoC has priority over nvimlsp
--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -1,62 +1,53 @@
 local language_server_list = require("language_server_list")
-local cocLSPs = language_server_list.coc
-
-local isStandard = true
-for _, x in ipairs(cocLSPs) do
-  if vim.o.filetype == x then
-    isStandard = false
-  end
-end
-
-if isStandard then
-  local saga = require "lspsaga"
-  local cmp = require "cmp"
-
-  local LSPs = language_server_list.nvimlsp
-  local nvim_lsp = require "lspconfig"
-  --local coq = require("coq")
-  local let = require("vim_interfaces/vars").let
-
-  saga.init_lsp_saga()
-
-  cmp.setup({
-      snippet = {
-        expand = function(args)
-          vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-        end
-      },
-
-      mapping = {
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<Tab>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-        ['<C-j>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-        ['<C-k>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.close(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        ['('] = cmp.mapping.confirm({ select = true }),
-      },
-
-      sources = {
-        {name = "nvim_lsp"},
-        {name = "vsnip"},
-      }
-    })
 
 
-  let.g("completion_matching_strategy_list", {"exact", "substring", "fuzzy"})
+local saga = require "lspsaga"
+local cmp = require "cmp"
 
-  local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local LSPs = language_server_list.lsp
+local nvim_lsp = require "lspconfig"
+--local coq = require("coq")
+local let = require("vim_interfaces/vars").let
 
-  for _, lsp in ipairs(LSPs) do
-    nvim_lsp[lsp].setup {
-      flags = {
-        debounce_text_changes = 150,
-      },
-      capabilities = capabilities
+saga.init_lsp_saga()
+
+cmp.setup({
+    snippet = {
+      expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      end
+    },
+
+    mapping = {
+      ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<Tab>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+      ['<C-j>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+      ['<C-k>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.close(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }),
+      ['('] = cmp.mapping.confirm({ select = true }),
+    },
+
+    sources = {
+      {name = "nvim_lsp"},
+      {name = "vsnip"},
     }
-  end
+  })
+
+
+let.g("completion_matching_strategy_list", {"exact", "substring", "fuzzy"})
+
+local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+for _, lsp in ipairs(LSPs) do
+  nvim_lsp[lsp].setup {
+    flags = {
+      debounce_text_changes = 150,
+    },
+    capabilities = capabilities
+  }
 end
 
 
